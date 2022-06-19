d6ee5373a4bd6cf95c43dbb5ec38ba9362a8481c
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-06-19 15:40:59 -0300

    [UPDATE] webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index',
@@ -20,5 +21,12 @@ module.exports = {
          }
        }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
 }
