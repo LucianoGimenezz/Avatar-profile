5e5228557face604ec998bbf1c788648d599e9de
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-06-21 17:47:34 -0300

    [ADD] Loader images, and style finished

--- src/templates/Card.js
+++ src/templates/Card.js
@@ -1,16 +1,37 @@
+import gitHub from '../assets/icons/github1.png';
+import profile from '../assets/images/perfilPng.png';
+
 const Card = () => {
     const view = `
     <section class="card">
         <div class="card__image">
-            <span></span>
+            <span>
+              <img src="${profile}"/>
+            </span>
         </div>
         <div class="card__info">
             <p class="card__about">Hi, My name is Luciano Gimenez</p>
             <h3 class="card__title">Front-end</h3>
+            <div class="rule"></div>
             <div class="icons-container">
-                <span>Twitter</span>
-                <span>Github</span>
-                <span>Linkedin</span>
+                <a>
+                  <img 
+                   src="assets/icons/twitter1.png"
+                   alt="Logo de twitter"
+                   />
+                </a>
+                <a>
+                 <img 
+                    src="${gitHub}" 
+                    alt="Logo de github"
+                    /> 
+                </a>
+                <a>
+                  <img
+                     src="assets/icons/linkedin1.png"
+                     alt="Logo de linkedin"
+                    />
+                </a>
             </div>
         </div>
     </section>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index',
@@ -27,7 +28,14 @@ module.exports = {
             MiniCssExtractPlugin.loader,
             'css-loader'
          ]
-       }
+       },
+      {
+        test:/\.(png|jpg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'src/assets[hash][ext]'
+        }
+      }
     ]
   },
   plugins: [
@@ -37,5 +45,17 @@ module.exports = {
       filename: './index.html'
     }),
     new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({ 
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/icons'),
+          to: 'assets/icons'
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/images'),
+          to: 'assets/images'
+        }
+      ]
+    })
   ]
 }
