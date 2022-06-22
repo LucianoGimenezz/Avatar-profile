8b3c9ed232d280ab9d90759cedb7dcfc876e2b61
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-06-22 12:22:33 -0300

    [ADD] README and netlify config

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso webpack
+Un avatar creado para el curso de webpack de platzi
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import router from '../src/routes/index';
 import '@styles/styles.css';
-console.log('Probando la variable de entorno:', process.env.ALGO);
 
 window.addEventListener('load',router);
 
