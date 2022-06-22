bc160cff7af443c2742066cddd4aeb617eb21a1d
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-06-22 12:03:39 -0300

    [ADD] Dark Mode

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import '@styles/styles.css';
 console.log('Probando la variable de entorno:', process.env.ALGO);
 
 window.addEventListener('load',router);
+
--- src/templates/Card.js
+++ src/templates/Card.js
@@ -1,10 +1,15 @@
 import gitHub from '@icons/github1.png';
 import profile from '@images/perfilPng.png';
+import sunIcon from '@icons/sun-icon- 1.png'
+import moonIcon from '@icons/moon-icon 1.png'
 
 const Card = () => {
     const view = `
     <section class="card">
         <div class="card__image">
+            <div class="change_mode">
+                <img id="icon_mode" src="${sunIcon}" alt="icon" />
+            </div>
             <span>
               <img src="${profile}"/>
             </span>
@@ -39,5 +44,32 @@ const Card = () => {
 
     return view;
 }
+
+window.addEventListener('load',() => {
+    setTimeout(() =>{
+        const button = document.querySelector('.change_mode');
+        button.addEventListener('click', () => {
+            const body = document.querySelector('body');
+            const div1 = document.querySelector('.card__image');
+            const div2 = document.querySelector('.card__info');
+            const buttonChangeMode = document.querySelector('.change_mode');
+            const iconMode = document.querySelector('#icon_mode');
+
+            if(!body.classList.value.includes('dark-mode')){
+                 body.classList.add('dark-mode');   
+                 div1.classList.add('card__image--dark');
+                 div2.classList.add('card__info--dark');
+                 buttonChangeMode.classList.add('change_mode--dark')
+                 iconMode.src = moonIcon;
+            }else {
+                body.classList.remove('dark-mode');   
+                div1.classList.remove('card__image--dark');
+                div2.classList.remove('card__info--dark');
+                buttonChangeMode.classList.remove('change_mode--dark')
+                iconMode.src = sunIcon;
+            }
+        })
+    } ,1000)
+})
 
 export default Card;
